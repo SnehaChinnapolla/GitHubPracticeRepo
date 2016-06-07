11d5499074054bd843c2201860c2f9c87ab26ae4
sneha.chinnapolla@mitsind.com <csnehachowdary@gmail.com>
2016-06-07 16:25:21 +0800

    Added a method in the class PracticeProgram and committing now

--- GitHubPractice/src/com/github/practice/PracticeProgram.java
+++ GitHubPractice/src/com/github/practice/PracticeProgram.java
@@ -3,6 +3,10 @@ package com.github.practice;
 public class PracticeProgram 
 {
 
+	void rePushing()
+	{
+		System.out.println("now modifying the current program by adding a method");
+	}
 	public static void main(String[] args)
 	{
 
