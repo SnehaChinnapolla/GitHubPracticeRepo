5c6b1a94a14e3c6097c6e60cb4ae7f007d0de48f
sneha.chinnapolla@mitsind.com <csnehachowdary@gmail.com>
2016-06-07 15:20:15 +0800

    Signed-off-by: sneha.chinnapolla@mitsind.com <csnehachowdary@gmail.com>

---
+++ GitHubPractice/src/com/github/practice/PracticeGHProg.java
@@ -0,0 +1,32 @@
+package com.github.practice;
+
+public class PracticeGHProg 
+{
+
+	void indexing()
+	{
+		System.out.println("now adding to index ");
+		
+	}
+
+	void pushing()
+	{
+		System.out.println("now pushing the class to the local repository ");
+		
+	}
+
+	void committing()
+	{
+		System.out.println("now committing the class to the remote repository ");
+		
+	}
+
+	
+	public static void main(String[] args) 
+	{
+
+		System.out.println("In main method of PracticeGHProg class ");
+		
+	}
+
+}
---
+++ GitHubPractice/src/com/github/practice/PracticeProgram.java
@@ -0,0 +1,14 @@
+package com.github.practice;
+
+public class PracticeProgram 
+{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("After sharing the project to the local repository this class is created");
+		
+		
+	}
+
+}
